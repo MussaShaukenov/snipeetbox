445bb966123a7a06579e48bc96f5091ec2cf7065
Mussá Shaukenov <mussa@MacBook-Pro-Mussa.local>
2022-08-24 15:57:48 +0500

    finished chapter 3

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
 
@@ -19,44 +18,34 @@ func home(w http.ResponseWriter, r *http.Request) {
 		"./ui/html/home.page.html",
 		"./ui/html/footer.html",
 	}
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
+
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server error", 500)
+		app.serverError(w, err)
 		return
 	}
 
-	// We then use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute() represents
-	// dynamic data that we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server error", 500)
+		app.serverError(w, err)
 	}
 }
 
-// Add a showSnippet handler function
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id parameter from the query string and try to
-	// convert it to an integer using the strconv.Atoi() function. If it can't
-	// be converted to an integer, or the value is less than 1, we return a 404
-	// not found response.
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
-	// Use the fmt.Fprintf() function to interpolate the id value with our resp
-	// and write it to the http.ResponseWriter.
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-// Add a createSnippet handler function
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,56 +7,35 @@ import (
 	"os"
 )
 
-func main() {
-	// Define a new command-line flag with the name 'addr', a default value of 4000
-	// and some short help text explaining what the flag controls. The value of
-	// flag will be stored in the addr variable at runtime.
-	addr := flag.String("addr", "4000", "HTTP network address")
+// Define an application struct to hold the application-wide dependencies for the
+// web application. For now, we'll only include fields for the two custom logger
+// we'll add more to it as the build progresses.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
-	// Importantly, we use the flag.Parse() function to parse the command-line
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any error
-	// encountered during parsing the application will be terminated.
+func main() {
+
+	addr := flag.String("addr", "4000", "HTTP network address")
 	flag.Parse()
 
-	// Use log.New() to create a logger for writing information messages. This
-	// three parameters: the destination to write the logs to (os.Stdout), a st
-	// prefix for message (INFO followed by a tab), and flags to indicate what
-	// additional information to include (local date and time). Note that the flags
-	// are joined using the bitwise OR operator |.
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-
-	// Create a logger for writing error messages in the same way, but use stde
-	// the destination and use the log.Lshortfile flag to include the relevant
-	// file name and line number.
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
 
-	//Use the http.NewServeMux() function to initialize a new servemux(handler), then
-	// register the home function as the handler for the "/" URL pattern.
-	// Register 2 new handler functions
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	// Initialize a new instance of application containing the dependencies.
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	// Create a file server which serves files out of the "./ui/static" directory
-	// Note that the path given to the http.Dir function is relative to the pro
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-	// Use the mux.Handle() function to register the file server as the handler
-	// all URL paths that start with "/static/". For matching paths, we strip t
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Write messages using the two new loggers, instead of the standard logger
 	infoLog.Printf("Starting server on %s", *addr)
-
-	//Use the http.ListenAndServe() function to start a new web server. We pass
-	// two parameters: the TCP network address to listen on (in this case ":4000"
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit.
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }

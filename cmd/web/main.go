b22b987e6195d9e41ea5a5ba1ed88e6f72c00859
Mussá Shaukenov <mussa@MacBook-Pro-Mussa.local>
2022-08-24 13:55:56 +0500

    added custom loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux(handler), then
+	// Define a new command-line flag with the name 'addr', a default value of 4000
+	// and some short help text explaining what the flag controls. The value of
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", "4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any error
+	// encountered during parsing the application will be terminated.
+	flag.Parse()
+
+	// Use log.New() to create a logger for writing information messages. This
+	// three parameters: the destination to write the logs to (os.Stdout), a st
+	// prefix for message (INFO followed by a tab), and flags to indicate what
+	// additional information to include (local date and time). Note that the flags
+	// are joined using the bitwise OR operator |.
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create a logger for writing error messages in the same way, but use stde
+	// the destination and use the log.Lshortfile flag to include the relevant
+	// file name and line number.
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime)
+
+	//Use the http.NewServeMux() function to initialize a new servemux(handler), then
 	// register the home function as the handler for the "/" URL pattern.
 	// Register 2 new handler functions
 	mux := http.NewServeMux()
@@ -24,11 +50,13 @@ func main() {
 	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass
+	// Write messages using the two new loggers, instead of the standard logger
+	infoLog.Printf("Starting server on %s", *addr)
+
+	//Use the http.ListenAndServe() function to start a new web server. We pass
 	// two parameters: the TCP network address to listen on (in this case ":4000"
 	// and the servemux we just created. If http.ListenAndServe() returns an error
 	// we use the log.Fatal() function to log the error message and exit.
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
